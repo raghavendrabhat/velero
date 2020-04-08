c0ea639505dbeb94539ab7c981e654449363e6ef
Raghavendra M <raghavendra@redhat.com>
2020-04-08 13:47:09 -0400

    clarify the wording on restore describe for namespaces included
    Signed-off-by: Raghavendra M <raghavendra@redhat.com>

--- pkg/cmd/util/output/restore_describer.go
+++ pkg/cmd/util/output/restore_describer.go
@@ -65,7 +65,7 @@ func DescribeRestore(restore *v1.Restore, podVolumeRestores []v1.PodVolumeRestor
 		d.Printf("Namespaces:\n")
 		var s string
 		if len(restore.Spec.IncludedNamespaces) == 0 {
-			s = "*"
+			s = "All namespaces found in the backup"
 		} else {
 			s = strings.Join(restore.Spec.IncludedNamespaces, ", ")
 		}
